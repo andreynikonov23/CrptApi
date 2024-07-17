080773e29e83980cdb481db2ba7dc0e6dd5e851c
Andrey <xxxkeep3rxxx@gmail.com>
2024-07-17 22:40:52 +0300

    17

--- src/main/java/ismp/crpt/ru/CrptApi.java
+++ src/main/java/ismp/crpt/ru/CrptApi.java
@@ -1,17 +1,48 @@
 package ismp.crpt.ru;
 
-import com.sun.net.httpserver.HttpServer;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class CrptApi {
-    public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create();
-        httpServer.bind(new InetSocketAddress("localhost", 8000), 2);
-        httpServer.createContext("/ismp.crpt.ru/api/", exchange -> {
-            System.out.println("Hello World!");
-        });
-        httpServer.start();
+    public CrptApi(){}
+    public CrptApi(TimeUnit timeUnit, int requestLimit) {
+
+    }
+
+    public Document createDoc() {
+        return new Document();
+    }
+
+    @JsonAutoDetect
+    class Document {
+        private String id;
+        private String status;
+        private DocType type;
+        private boolean importRequest;
+        private String ownerInn;
+        private String participantInn;
+        private List<Product> products;
+        private Date regDate;
+        private String regNumber;
+
+    }
+    @JsonAutoDetect
+    class Product {
+        private String certificateDocument;
+        private Date certificateDate;
+        private String certificateDocumentNumber;
+        private String ownerInn;
+        private String producerInn;
+        private Date productionDate;
+        private String tnvedCode;
+        private String uitCode;
+        private String uituCode;
+    }
+    @JsonAutoDetect
+    enum DocType {
+        LP_INTRODUCE_GOODS;
     }
 }
---
+++ src/main/java/ismp/crpt/ru/Main.java
@@ -0,0 +1,18 @@
+package ismp.crpt.ru;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create();
+        httpServer.bind(new InetSocketAddress("localhost", 8000), 2);
+        httpServer.createContext("/ismp.crpt.ru/api/", exchange -> {
+            System.out.println("Hello World!");
+        });
+        httpServer.start();
+
+    }
+}
